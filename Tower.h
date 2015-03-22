24e058675741fee6c493e3c4c5bb70197dc6865b
Jack Ryan <johnmryan830@gmail.com>
2015-03-21 21:37:02 -0400

    Create Tower.h

---
+++ Tower.h
@@ -0,0 +1,27 @@
+/* Jack Ryan
+ * CSE 20212
+ * Final Project
+ * Tower.h
+ * Tower interface for abstract base class
+ */
+#ifndef TOWER_H
+#define TOWER_H
+
+#include <iostream>
+using namespace std;
+
+class Tower {
+public:
+        virtual void target();  //targeting function, can be implemented by subclasses for multitargeting (cannon)
+        virtual void attack();  //function to begin attacking (decreasing health) of target
+
+        void displayTower(Image &);     //displays a tower with its specific image
+
+        virtual double getDPS() = 0;    //returns damage times attack speed (damage per second)
+        virtual double getRadius() = 0; //returns distance the tower can fire
+        virtual double getCost() = 0;   //returns cost to build tower
+private:
+        unsigned double TowerSize;      //holds value for size of a tower
+};
+
+#endif
