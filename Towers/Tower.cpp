0d15c4f150087b515d415b7ee4d1bf5d8dc1214a
Luke Garrison <lgarriso@nd.edu>
2015-04-21 00:49:32 -0400

    Towers will now lock onto an enemy until out of range

--- Towers/Tower.cpp
+++ Towers/Tower.cpp
@@ -10,20 +10,31 @@ Tower::Tower(SDL_Renderer** gRendererPtr) : Object(gRendererPtr) {
 	gRendererr = gRendererPtr;
 	MAX_DIMENSION = 70;
 	MAX_DISTORTION = .57;
+	target = NULL;
 }
 
 
 bool Tower::inRange(vector<Enemy> *enemies)
 {
+	if(target != NULL) {
+		double distance = sqrt(pow(target->getPosY() - towerY, 2.0) + pow(target->getPosX() - towerX, 2.0));
+		if(distance <= range) {
+			return true; // return true if already locked onto enemy && the enemy is still in range
+		}
+	}
+
 	for (int i=0;i<enemies->size();i++) {
-		double enemy_X = (*enemies)[i].getPosX();
-		double enemy_Y = (*enemies)[i].getPosY();
-		double distance = sqrt(pow(enemy_Y - TOWER_Y, 2.0) + pow(enemy_X - TOWER_X, 2.0));
-		if (distance <= TOWER_RANGE)
+		int enemy_X = (*enemies)[i].getPosX();
+		int enemy_Y = (*enemies)[i].getPosY();
+		double distance = sqrt(pow(enemy_Y - towerY, 2.0) + pow(enemy_X - towerX, 2.0));
+		if (distance <= range) {
+			target = &((*enemies)[i]);
 			return true;
+		}
 	}
 	return false;
 }
+
 //int Tower::getCost() {
 	
 //};
